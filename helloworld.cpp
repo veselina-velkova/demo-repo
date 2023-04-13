b75201bf32082cd9888ab5091bad4ceb9996696f
Yolina Petkova <yolina.petkova@strypes.eu>
2023-04-13 12:39:33 +0300

    -Adding new function called yolina()

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 
+void yolina()
+{
+    std::cout<<"Hello, my name is Yolina!"<<std::endl;
+}
+
 int main()
 {
     std::cout<<"Hello, World!"<<std::endl;
+
+    yolina();
+
     return 0;
 }
