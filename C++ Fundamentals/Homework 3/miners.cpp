b3fcd30c160c76520b2afc53d36e4b34a644d056
veselina-velkova <veselina.velkova@strypes.eu>
2023-05-23 13:27:59 +0300

    Added solution to problem 3

---
+++ C++ Fundamentals/Homework 3/miners.cpp
@@ -0,0 +1,52 @@
+#include<map>
+#include<string>
+#include<queue>
+#include<iostream>
+
+const std::string STOP_WHILE_LOOP = "stop";
+constexpr int EMPTY = 0;
+
+//Here I am not sure is it a good idea to read the input and sum the quantities in the same function.
+//But like this we can use the functionalities of the map.
+std::map<std::string, int> extractInput(std::queue<std::string>& keepOrderResources)
+{
+    std::map<std::string, int> resources{};
+    while(true)
+    {
+        std::string resource{};
+        int quantity{};
+        std::cin >> resource;
+        if(resource == STOP_WHILE_LOOP)
+        {
+            break;
+        }
+        std::cin >> quantity;
+        if(resources[resource] == EMPTY)
+        {
+            keepOrderResources.push(resource);
+        }
+        resources[resource] += quantity;
+    }
+    return resources;
+}
+
+//Here we can empty the real queue and there is no problem, because its function was just to help us.
+void print(const std::map<std::string, int>& results, std::queue<std::string>& keepOrder)
+{
+    while(!keepOrder.empty())
+    {
+        std::string resource = keepOrder.front();
+        std::cout << resource << " -> " << results.at(resource) << '\n';
+        keepOrder.pop();
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::queue<std::string> keepOrderResources{};
+    std::map<std::string, int> input = extractInput(keepOrderResources);
+    print(input, keepOrderResources);
+
+    return 0;
+}
