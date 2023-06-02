51f55d0c64f9303e37d643829f784026813c547a
veselina-velkova <veselina.velkova@strypes.eu>
2023-06-02 11:23:07 +0300

    Added doxy header

--- C++ Fundamentals/Final project/code_vs_zombies.cpp
+++ C++ Fundamentals/Final project/code_vs_zombies.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file code_vs_zombies.cpp
+ * @author Veselina Velkova (veselina.velkova@strypes.eu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ ****************************************************/
+
 #include <vector>
 #include <cmath>
 #include <algorithm>
